a5c6d666c51b143c707285fe3682d13cdac70b7c
Mansiiiiiiiiiiiiiiiii <mansi15094@gmail.com>
2025-06-06 18:24:22 +0530

    Node JS

---
+++ app.js
@@ -0,0 +1,22 @@
+const amount = 12;
+
+if(amount<10){
+    console.log('small number')
+}
+else{
+    console.log('larger number')
+}
+console.log(`hay it's my first node app!!!`)
+
+//Modules
+
+const chetna = 'chetna'
+const ujjwal = 'ujjwal'
+
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`)
+}
+
+sayHi('Lavina')
+sayHi(chetna)
+sayHi(ujjwal)
---
+++ global.js
@@ -0,0 +1,8 @@
+//gloabals
+
+console.log(__dirname)
+
+setInterval(() => {
+    console.log('mansi')
+}, 1000
+)
